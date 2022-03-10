a190dc93b10cba0a6864db2e0efaa46d7d0d4e73
Ryan Mburu <mbururyan31@gmail.com>
2022-03-10 17:08:04 +0300

    made changes to script

--- random_hero_SWBF2.py
+++ random_hero_SWBF2.py
@@ -1,14 +1,30 @@
 # Random number for picking heroes in Battlefront
 import random
 
+# Chose faction between Hero or Villain
+
+faction = input('You want a Hero or a Villain? : ')
+print(faction)
+
+# If statement
+
+if faction == 'Hero':
+    heroes = ['luke', 'leia', 'han', 'chewbacca', 'lando', 'rey', 'anakin', 'obiwan', 'yoda', 'finn']
+    print('\nYour hero this round will be ... : ', random.choice(heroes))
+
+elif faction == 'Villain':
+    villains = ['boba', 'bossk', 'vader', 'palpatine', 'kylo', 'maul', 'iden', 'phasma', 'grievous', 'dooku']
+    print('\nYour villain this round will be ... : ', random.choice(villains))
+else:
+    print('Choose your faction again')
+
 # Lightside
 
-heroes = ['luke', 'leia', 'han', 'chewbacca', 'lando', 'rey', 'anakin', 'obiwan', 'yoda', 'finn']
-print('Lightside play : ', random.choice(heroes))
+
 
 
 # Villains
 
-villains = ['boba', 'bossk', 'vader', 'palpatine', 'kylo', 'maul', 'iden', 'phasma', 'grievous', 'dooku']
-print('\nDarkside play : ', random.choice(villains))
+# 
+# 
 
